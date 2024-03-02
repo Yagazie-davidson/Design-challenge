4dc3a7811953485c8f1b7ac4d795f305df8cb23f
letami0 <letam@izero.dev>
2024-03-02 21:02:48 +0100

    first push

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,271 @@
+"use client";
+import CompanyLogo from "@/assets/Placeholder-Company-Logos.png";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+
 import Image from "next/image";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="px-6 md:px-[118px]">
+      <h2 className="text-[#17171F] text-4xl font-semibold mb-8">Settings</h2>
+      <Tabs defaultValue="company-info" className="w-full">
+        <TabsList>
+          <TabsTrigger value="your-profile">Your Profile</TabsTrigger>
+          <TabsTrigger value="company-info">Company Info</TabsTrigger>
+          <TabsTrigger value="manage-seats">Manage Seats</TabsTrigger>
+        </TabsList>
+        <TabsContent value="your-profile">Your Profile</TabsContent>
+        <TabsContent value="company-info">
+          <CompanyInfoForm />
+        </TabsContent>
+        <TabsContent value="manage-seats">Manage seats</TabsContent>
+      </Tabs>
     </main>
   );
 }
+
+const CompanyInfoForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  return (
+    <form className="w-full mt-8 md:mt-14" onSubmit={handleSubmit(onSubmit)}>
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center md:justify-between md:items-start w-full">
+        <div className="flex items-center gap-x-8 justify-center">
+          <Image
+            src={CompanyLogo}
+            alt="Company Logo"
+            loading="eager" //This is for better lighthouse performance score
+          />
+          <div>
+            <div className="flex items-center gap-x-3">
+              <Button variant="destructive" aria-label="remove company logo">
+                Remove
+              </Button>
+              <Button variant="outline" aria-label="change company logo">
+                Change Photo
+              </Button>
+            </div>
+            <p className="text-sm text-[#17171F66]">
+              or drag and drop (SVG, PNG, JPG)
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center justify-center md:justify-end gap-x-3 order-first mb-7 md:mb-0 md:order-last">
+          <Button variant="outline" aria-label="cancel">
+            Cancel
+          </Button>
+          <Button aria-label="save changes" type="submit">
+            Save changes
+          </Button>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-6 mt-12">
+        {/* Input Group */}
+        {/* Company Name */}
+        <div className="grid w-full items-center gap-1.5">
+          <Label htmlFor="company-name" className="text-[#17171FCC]">
+            Company's Name
+          </Label>
+          <Input
+            type="text"
+            id="company-name"
+            {...register("company_name", {
+              required: "This field is required",
+            })}
+          />
+          {errors.company_name && (
+            <p className="text-red-600">{errors.company_name.message}</p>
+          )}
+        </div>
+        {/* Input Group */}
+        {/* Company's Website */}
+        <div className="grid w-full items-center gap-1.5">
+          <Label htmlFor="company-website" className="text-[#17171FCC]">
+            Company's Website
+          </Label>
+          <Input
+            type="text"
+            id="company-website"
+            {...register("company_website", {
+              required: "This field is required",
+            })}
+          />
+          {errors.company_website && (
+            <p className="text-red-600">{errors.company_website.message}</p>
+          )}
+        </div>
+        {/* Input Group */}
+        {/* Company's LinkedIn */}
+        <div className="grid w-full items-center gap-1.5">
+          <Label htmlFor="company-linkedin" className="text-[#17171FCC]">
+            Company's Linkedin
+          </Label>
+          <Input
+            type="text"
+            id="company-linkedin"
+            placeholder=""
+            {...register("company_linkedin", {
+              required: "This field is required",
+            })}
+          />
+          {errors.company_linkedin && (
+            <p className="text-red-600">{errors.company_linkedin.message}</p>
+          )}
+        </div>
+        {/* Input Group */}
+        {/* Company's Industry */}
+        <div className="grid w-full items-center gap-1.5">
+          <Label htmlFor="company-industry" className="text-[#17171FCC]">
+            Company's Industry
+          </Label>
+          <Input
+            type="text"
+            id="company-industry"
+            placeholder=""
+            {...register("company_industry", {
+              required: "This field is required",
+            })}
+          />
+          {errors.company_industry && (
+            <p className="text-red-600">{errors.company_industry.message}</p>
+          )}
+        </div>
+      </div>
+      {/* Input Group */}
+      {/* Employee count */}
+      <div className="grid w-full items-center gap-1.5 mt-6">
+        <Label htmlFor="employee-count" className="text-[#17171FCC]">
+          Employee Count
+        </Label>
+        <div className="flex items-center gap-x-3 flex-wrap gap-y-2">
+          <div className="py-2 px-3 border max-w-fit rounded-full bg-black text-white cursor-pointer">
+            1-10
+          </div>
+          <div className="py-2 px-3 border max-w-fit rounded-full cursor-pointer">
+            10-100
+          </div>
+          <div className="py-2 px-3 border max-w-fit rounded-full cursor-pointer">
+            100-500
+          </div>
+          <div className="py-2 px-3 border max-w-fit rounded-full cursor-pointer">
+            1000+
+          </div>
+        </div>
+      </div>
+      {/* Input Group */}
+      {/* Company's description */}
+      <div className="grid w-full items-center gap-1.5 mt-6">
+        <Label htmlFor="company-description" className="text-[#17171FCC]">
+          Company's description
+        </Label>
+        <Input
+          type="text"
+          id="company-description"
+          placeholder=""
+          {...register("company_description", {
+            required: "This field is required",
+          })}
+        />
+        <p className="text-sm font-medium text-[#17171F99]">
+          Your detailed company description
+        </p>
+        {errors.company_description && (
+          <p className="text-red-600">{errors.company_description.message}</p>
+        )}
+      </div>
+      {/* Input Group */}
+      {/* Company Goals */}
+      <div className="grid w-full items-center gap-1.5 mt-6">
+        <Label htmlFor="company-goals" className="text-[#17171FCC]">
+          What are your company goals?
+        </Label>
+        <Input
+          type="text"
+          id="company-goals"
+          placeholder=""
+          {...register("company_goals", {
+            required: "This field is required",
+          })}
+        />
+        {errors.company_goals && (
+          <p className="text-red-600">{errors.company_goals.message}</p>
+        )}
+      </div>
+      {/* Input Group */}
+      {/* Headquarters */}
+      <div className="grid w-full items-center gap-1.5 mt-6">
+        <Label htmlFor="headquarters" className="text-[#17171FCC]">
+          Headquarters
+        </Label>
+        <Input
+          type="text"
+          id="headquarters"
+          placeholder=""
+          {...register("headquarters", {
+            required: "This field is required",
+          })}
+        />
+        {errors.headquarters && (
+          <p className="text-red-600">{errors.headquarters.message}</p>
+        )}
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-6 mt-6">
+        {/* Input Group */}
+        {/* Funding Round */}
+        <div className="grid w-full items-center gap-1.5">
+          <Label htmlFor="funding-round" className="text-[#17171FCC]">
+            Funding Round
+          </Label>
+          <Select
+            {...register("funding_round", {
+              required: "This field is required",
+            })}
+          >
+            <SelectTrigger className="w-full" aria-label="select funding round">
+              <SelectValue placeholder="Seed" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="seed">Seed</SelectItem>
+              <SelectItem value="otherA">Other A</SelectItem>
+              <SelectItem value="otherB">Other B</SelectItem>
+            </SelectContent>
+          </Select>
+          {errors.funding_round && (
+            <p className="text-red-600">{errors.funding_round.message}</p>
+          )}
+        </div>
+        {/* Input Group */}
+        {/* FAQs */}
+        <div className="grid w-full items-center gap-1.5">
+          <Label htmlFor="FAQs" className="text-[#17171FCC]">
+            FAQs
+          </Label>
+          <Input
+            type="text"
+            id="FAQs"
+            placeholder=""
+            {...register("faqs", {
+              required: "This field is required",
+            })}
+          />
+          {errors.faqs && <p className="text-red-600">{errors.faqs.message}</p>}
+        </div>
+      </div>
+    </form>
+  );
+};
